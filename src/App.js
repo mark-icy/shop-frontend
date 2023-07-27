bb27baa9f7dabf1f85d81f46ff812ca0de384aa4
Catherine Montano <catherine@celestia.local>
2023-07-27 07:05:09 -0700

    login --- tama ba git commit ko?

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Items from "./components/Items.js";
 import Footer from "./components/Footer.js";
 import Banner from "./components/Banner.js";
+import Login from "./components/Login.js";
 
 function App(){
   return (
 	<div className="App">
 		<Header/>
 	  	<Banner/>
+		<Login/>
 	  	<Items/>
             	<Footer/>
 	</div>
