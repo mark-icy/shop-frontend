4c339aebee77f0df9c8376ba015f32df3c362559
Catherine Montano <catherine@celestia.local>
2023-07-31 13:41:49 -0700

    changes to login component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer.js";
 import Banner from "./components/Banner.js";
 import Login from "./components/Login.js";
 
+
 function App(){
   return (
 	<div className="App">
@@ -12,6 +13,7 @@ function App(){
 	  	<Banner/>
 		<Login/>
 	  	<Items/>
+		
             	<Footer/>
 	</div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,29 @@
 import './Login.css';
+import Button from './Button';
 
 const Login = () => {
 	return (
-        <div className='login'></div>
+        <div className='login'>
+			<div className='login-forms'>
+            <div className= "login-form">
+				<form>
+					<label>
+						<input type="e-mail" name="name"/>
+					</label>
+				</form>
+			</div>
+			<div className='password-form'>
+				<form>
+					<label>
+						<input type="password" name="password"/>
+					</label>
+				</form>
+			</div>
+			<Button className="login-button" name="login"/>
+			</div>
+		</div>
+
+        
 	)
 }
 
